04a43b44690f669c14cf61e18ba7831b8d92087f
brenda70904 <brendaj0214@gmail.com>
2022-11-14 15:11:39 -0800

    set up repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  
+  handleSubmit = (e) => {
+    e.preventDefault
+  }
+  render(){
+    return(
+      <>
+        <h1> City Explorer </h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>Search for City
+            <input type="text" name="city" placeholder="enter city..."/>
+            </label>
+            <button type="submit" >Search a City</button>
+            </form>
+      </>
+    )
+  }
+  
 }
-
-export default App;
