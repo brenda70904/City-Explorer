e60065f67c5e8a6cf6d316dca72a7cf196d7b7ea
brenda70904 <brendaj0214@gmail.com>
2022-11-14 16:12:58 -0800

    update README.md

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class App extends React.Component{
   
   handleSubmit = (e) => {
-    e.preventDefault
+    e.preventDefault();
   }
   render(){
     return(
